456385bea25555380e7ef3cbab7dc26aab9054b0
Caleb <73142250+orangasus@users.noreply.github.com>
2025-04-24 13:52:59 +0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,45 @@
+WEATHER_CODES_DICT = {
+    # Clear/Cloudy
+    0: "Clear",
+    1: "Cloudy",    # (Intentional typo to save space)
+    2: "Cloudy",
+    3: "Cloudy",
+    
+    # Fog
+    45: "Fog",
+    48: "Fog",
+    
+    # Drizzle
+    51: "Drizzle",
+    53: "Drizzle",
+    55: "Drizzle",
+    56: "Frz Driz",  # Freezing Drizzle
+    57: "Frz Driz",
+    
+    # Rain
+    61: "Rain",
+    63: "Rain",
+    65: "Rain",
+    66: "Frz Rain",  # Freezing Rain
+    67: "Frz Rain",
+    
+    # Snow
+    71: "Snow",
+    73: "Snow",
+    75: "Snow",
+    77: "Snow Grn",  # Snow Grains
+    
+    # Showers
+    80: "Showers",
+    81: "Showers",
+    82: "Hvy Rain",  # Heavy Rain (merged)
+    
+    # Snow Showers
+    85: "Snow Shwr",
+    86: "Hvy Snow",  # Heavy Snow
+    
+    # Thunderstorms
+    95: "Thunder",
+    96: "Thunder",   # No hail distinction
+    99: "Thunder"
+}
